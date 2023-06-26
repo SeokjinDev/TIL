27ea6f7a243a7b5bd362a03840859bf7579d7658
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-06-26 18:12:59 +0900

    [Silver V] Title: 막대기, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1094. 막대기/막대기.py
+++ 백준/Silver/1094. 막대기/막대기.py
@@ -1,10 +1,1 @@
-x = int(input())
-count = 0
-if x % 2:
-    count += 1
-    x -= 1
-for i in range(6):
-    if x >= 2**(6-i):
-        x -= 2**(6-i)
-        count += 1
-print(count)
+print(str(bin(int(input()))).count('1'))
