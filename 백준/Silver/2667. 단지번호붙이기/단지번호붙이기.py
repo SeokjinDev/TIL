7c4553c93dcfe879dd020a432caf1f8727ffe444
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-08-09 13:20:54 +0900

    [Silver I] Title: 단지번호붙이기, Time: 48 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -1,28 +1,22 @@
-from collections import deque
-
 n = int(input())
-square = [[*input()] for _ in range(n)]
-
-def dfs(y, x, m):
-    queue = deque([[y, x]])
+m = [[*input()] for _ in range(n)]
+def bfs(y, x):
+    stack = [[y, x]]
     m[y][x] = '0'
     count = 1
-    while queue:
-        ny, nx = queue.popleft()
+    while stack:
+        ny, nx = stack.pop()
         for my, mx in zip([-1, 1, 0, 0], [0, 0, -1, 1]):
             ty, tx = ny + my, nx + mx
-            if n > ty >= 0 and n > tx >= 0:
-                if m[ty][tx] == '1':
-                    queue.append([ty, tx])
-                    m[ty][tx] = '0'
-                    count += 1
-    return m, count
-
+            if n > ty >= 0 and n > tx >= 0 and m[ty][tx] == '1':
+                stack.append([ty, tx])
+                m[ty][tx] = '0'
+                count += 1
+    return count
 result = []
 for i in range(n):
     for j in range(n):
-        if square[i][j] == '1':
-            square, d = dfs(i, j, square)
-            result.append(d)
+        if m[i][j] == '1':
+            result.append(bfs(i, j))
 print(len(result))
 [print(r) for r in sorted(result)]
