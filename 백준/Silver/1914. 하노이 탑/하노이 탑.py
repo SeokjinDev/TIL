d3d102d8be1a4ae5d0080536f2a74053f5901672
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-08-17 02:37:56 +0900

    [Silver I] Title: 하노이 탑, Time: 876 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Silver/1914. 하노이 탑/하노이 탑.py
+++ 백준/Silver/1914. 하노이 탑/하노이 탑.py
@@ -1,11 +1,8 @@
-def hanoi(n, s, m, e):
-    if n == 1:
-        print(s, e)
-        return
-    hanoi(n-1, s, e, m)
-    hanoi(1, s, m, e)
-    hanoi(n-1, m, s, e)
-n = int(input())
+def f(n,s,m,e):
+    if n==1:return print(s,e)
+    f(n-1,s,e,m)
+    f(1,s,m,e)
+    f(n-1,m,s,e)
+n=int(input())
 print(2**n-1)
-if n <= 20:
-    hanoi(n, 1, 2, 3)
+if n<21:f(n,1,2,3)
