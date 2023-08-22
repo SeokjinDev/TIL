3c748c830266f9b7602efbffd40bede31e7c76e1
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-08-22 22:01:37 +0900

    [Silver V] Title: K-세준수, Time: 144 ms, Memory: 32820 KB -BaekjoonHub

--- 백준/Silver/1418. K－세준수/K－세준수.py
+++ 백준/Silver/1418. K－세준수/K－세준수.py
@@ -1,13 +1,10 @@
 n = int(input())
 m = int(input())
 nums = [0] * (n+1)
-
 for i in range(2, n+1):
     if nums[i] == 0:
         for j in range(i, n+1, i):
             if j % i == 0:
                 nums[j] = max(nums[j], i)
-ans = 0
-for i in nums:
-    if i <= m: ans += 1
-print(ans-1)
+ans = sum([1 if i <= m else 0 for i in nums])-1
+print(ans)
