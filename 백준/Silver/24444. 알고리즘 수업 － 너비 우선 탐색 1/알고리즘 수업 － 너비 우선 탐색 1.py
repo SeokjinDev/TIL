af175c99d7b7032e557228406451b64a3e4418fb
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-08-23 23:09:29 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 688 ms, Memory: 66860 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.py
@@ -0,0 +1,23 @@
+from collections import deque
+import sys
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+graph = {}
+for _ in range(m):
+    u, v = map(int, input().split())
+    if graph.get(u): graph[u].append(v)
+    else: graph[u] = [v]
+    if graph.get(v): graph[v].append(u)
+    else: graph[v] = [u]
+
+cnt = 1
+visited = [0] * (n+1)
+queue = deque(sorted(graph[r]))
+visited[r] = cnt
+while queue:
+    data = queue.popleft()
+    if visited[data] == 0:
+        cnt += 1
+        visited[data] = cnt
+        queue.extend(sorted(graph[data]))
+[print(v) for v in visited[1:]]
