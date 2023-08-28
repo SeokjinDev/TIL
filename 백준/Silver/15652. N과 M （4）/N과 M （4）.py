a0caed1f80c7713853cb107e3c30797d70ba54b4
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-08-29 05:11:08 +0900

    [Silver III] Title: N과 M (4), Time: 76 ms, Memory: 32276 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.py
@@ -0,0 +1,14 @@
+n, m = map(int, input().split())
+ans = []
+
+def dfs(x, l, r):
+    global ans
+    if l == 0:
+        ans.append(r)
+        return
+    for i in range(x, n+1):
+        dfs(i, l-1, r+[i])
+dfs(1, m, [])
+for an in ans:
+    [print(a, end=' ') for a in an]
+    print()
