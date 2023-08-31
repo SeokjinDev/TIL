c57e6ccba0667d8a4d00b29add3b23862255e389
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2023-09-01 00:55:41 +0900

    [Silver II] Title: N과 M (9), Time: 112 ms, Memory: 35868 KB -BaekjoonHub

--- 백준/Silver/15663. N과 M （9）/N과 M （9）.py
+++ 백준/Silver/15663. N과 M （9）/N과 M （9）.py
@@ -1,17 +1,5 @@
+from itertools import permutations
 n, m = map(int, input().split())
-ns = sorted([*map(int, input().split())])
-ans = {}
-def dfs(l, sequence, tmp):
-    global ans
-    if l == 0:
-        seq = ' '.join(map(str, sequence))
-        if ans.get(seq) is None:
-            ans[seq] = 1
-        return
-    for i in range(n):
-        if i not in tmp:
-            dfs(l-1, sequence+[ns[i]], tmp+[i])
-for i in range(n):
-    dfs(m, [], [])
-for a in ans.keys():
-    print(a)
+ns = [*map(int, input().split())]
+for p in sorted(set(permutations(ns, m))):
+    print(*p)
