dc31baba780f696ecf1b0e396e581df3ab46174e
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2024-07-03 07:41:34 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 3, Time: 608 ms, Memory: 78572 KB -BaekjoonHub

---
+++ 백준/Silver/24481. 알고리즘 수업 － 깊이 우선 탐색 3/알고리즘 수업 － 깊이 우선 탐색 3.py
@@ -0,0 +1,22 @@
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+n, m, r = map(int, input().split())
+graph = [[] for _ in range(n+1)]
+for _ in range(m):
+    u, v = map(int, input().split())
+    graph[u].append(v)
+    graph[v].append(u)
+
+visited = [-1] * (n+1)
+def dfs(r, idx):
+    global visited, graph
+    visited[r] = idx
+    for g in sorted(graph[r]):
+        if visited[g] == -1:
+            dfs(g, idx+1)
+    return
+
+dfs(r, 0)
+for i in visited[1:]:
+    print(i)
