8d717f9e7ae2a8bcd1a499a69cdfd93960003249
SeokjinDev <56934924+SeokjinDev@users.noreply.github.com>
2025-02-27 18:22:29 +0900

    [Silver V] Title: 임시 반장 정하기, Time: 648 ms, Memory: 100024 KB -BaekjoonHub

--- 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
+++ 백준/Silver/1268. 임시 반장 정하기/임시 반장 정하기.py
@@ -1,14 +1,8 @@
-t = int(input())
-b = [[*map(int, input().split())] for _ in range(t)]
-
-friends = {i: set() for i in range(t)}
-
-for i in range(t):
-    for j in range(5):
-        for k in range(i+1, t):
-            if b[i][j] == b[k][j]:
-                friends[i].add(k)
-                friends[k].add(i)
-
-cnt = [len(friends[i]) for i in range(t)]
+import sys
+inp = sys.stdin.readline
+t = int(inp())
+b = [[*map(int, inp().split())] for _ in range(t)]
+f = {i: set() for i in range(t)}
+[f[i].add(k) or f[k].add(i) for i in range(t) for j in range(5) for k in range(i+1, t) if b[i][j] == b[k][j]]
+cnt = [len(f[i]) for i in range(t)]
 print(cnt.index(max(cnt))+1)
